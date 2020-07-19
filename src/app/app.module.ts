33dbf98e5341a03672d96b4a2981890852f4c01e
Prabhu <prabhuadams983@gmail.com>
2020-07-19 17:11:28 +0530

    OVS-123-added-location-validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
 import { LocationTabComponent } from './admin/location-tab/location-tab.component';
 import { CandidateTabComponent } from './admin/candidate-tab/candidate-tab.component';
 import { UserTabComponent } from './admin/user-tab/user-tab.component';
@@ -15,7 +14,6 @@ import { UserTabComponent } from './admin/user-tab/user-tab.component';
     AppComponent,
     LoginComponent,
     AdminComponent,
-    UserComponent,
     LocationTabComponent,
     CandidateTabComponent,
     UserTabComponent
